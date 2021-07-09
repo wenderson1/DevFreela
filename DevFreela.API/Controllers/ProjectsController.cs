8764035fbc97098ffbf8d85eb3a42f8092aa9723
wenderson1 <wenderson@alunos.fho.edu.br>
2021-07-08 23:22:40 -0300

    Actions

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,61 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        //api/projects/id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //return NotFound();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+            //cadastrar
+            return CreatedAtAction(nameof(GetById),new { id = createProject.Id },createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        //api/projects/3
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            //Buscar, se nao existir, retornar Not Found
+
+            //Remover
+            return NoContent();
+        }
+
+    }
+}
