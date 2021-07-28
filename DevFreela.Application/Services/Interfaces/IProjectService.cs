68eb48ed879df2e9a5684be24317337c5d469c9e
Wenderson Farias <wenderson.farias20@gmail.com>
2021-07-27 22:08:29 -0300

    Camada Application

---
+++ DevFreela.Application/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+        ProjectDetailsViewModel GetById(int id);
+        int Create(NewProjectInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel)
+    }
+}
